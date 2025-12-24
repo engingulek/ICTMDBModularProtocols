3c10e059725c98eef082132e9bd524acb849fde1
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-12-25 00:47:08 +0300

    updated for SwiftUI

--- Sources/ICTMDBModularProtocols/ICTMDBModularProtocols.swift
+++ Sources/ICTMDBModularProtocols/ICTMDBModularProtocols.swift
@@ -1,16 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import UIKit
+import SwiftUI
 
 // MARK: - Home Module Protocol
-/// Defines the contract for creating the Home module’s view controller.
-/// Used to assemble and return the main screen of the application.
+/// Defines the contract for creating the Home module's view.
+/// Used to assemble and return the primary entry point of the application.
 public protocol HomeModuleProtocol {
     
-    /// Creates and returns the Home module’s main view controller.
-    /// - Returns: A configured `UIViewController` representing the Home screen.
-    func createHomeModule() -> UIViewController
+    /// Creates and returns the main view of the Home module.
+    /// - Returns: A configured SwiftUI `AnyView` representing the Home screen.
+    func createHomeModule() -> AnyView
 }
 
 // MARK: - All List Module Protocol
@@ -22,22 +22,22 @@ public enum AllListType {
 }
 
 /// A protocol that defines how to create the All List module
-/// based on a given list type (e.g., popular or airing today).
+/// based on a specific list type (e.g., popular or airing today).
 public protocol AllListModuleProtocol {
     
-    ///Creates and returns a configured All List module for the given type.
-    /// - Parameter type: The list type (e.g., `.popular` or `.airingToday`).
-    /// - Returns: A `UIViewController` configured to display the corresponding list.
-    func createAllListModule(type: AllListType) -> UIViewController
+    /// Creates and returns a configured All List view for the given type.
+    /// - Parameter type: The list category (e.g., `.popular` or `.airingToday`).
+    /// - Returns: A SwiftUI `AnyView` configured to display the corresponding list.
+    func createAllListModule(type: AllListType) -> AnyView
 }
 
 // MARK: - TV Show Detail Module Protocol
 /// A protocol responsible for creating and returning
-/// the detail screen of a selected TV show.
+/// the detail view for a specific TV show.
 public protocol TvShowDetailProtocol {
     
-    /// Creates and returns the detail view controller for a TV show.
+    /// Creates and returns the detail view for a TV show.
     /// - Parameter id: The unique identifier of the TV show (optional).
-    /// - Returns: A configured `UIViewController` displaying the TV show details.
-    func createTvShowDetailModule(id: Int?) -> UIViewController
+    /// - Returns: A configured SwiftUI `AnyView` displaying the TV show details.
+    func createTvShowDetailModule(id: Int?) -> AnyView
 }
